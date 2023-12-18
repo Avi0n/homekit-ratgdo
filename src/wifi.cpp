ccf3b38b1b9352b3e0ff2ae2948122e8306794d2
Avi0n <14863961+Avi0n@users.noreply.github.com>
2023-12-18 13:32:31 -0800

    vector sort

--- src/wifi.cpp
+++ src/wifi.cpp
@@ -159,16 +159,41 @@ bool on_command_callback(improv::ImprovCommand cmd) {
 void get_available_wifi_networks() {
     int networkNum = WiFi.scanNetworks();
 
-    for (int id = 0; id < networkNum; ++id) {
+    std::vector<int> sortedIndicies;
+    for (int i = 0; i < networkNum; i++) {
+        sortedIndicies.push_back(i);
+    }
+
+    // sort networks by RSSI, strongest to weakest
+    std::sort(sortedIndicies.begin(), sortedIndicies.end(),
+        [](const int& a, const int& b) { return WiFi.RSSI(a) > WiFi.RSSI(b); });
+
+    // find duplicates (must be RSSI sorted)
+    String temp_ssid;
+    for (int i = 0; i < networkNum; i++) {
+        if (sortedIndicies[i] == -1) continue;       // skip duplicate
+        temp_ssid = WiFi.SSID(sortedIndicies[i]);
+        for (int j = i + 1; j < networkNum; j++) {
+            if (temp_ssid == WiFi.SSID(sortedIndicies[j])) {
+                sortedIndicies[j] = -1;              // set dupes to -1 to skip later
+            }
+        }
+    }
+
+    for (size_t id = 0; id < sortedIndicies.size(); ++id) {
+        if (sortedIndicies[id] == -1) continue;      // skip duplicate
         std::vector<uint8_t> data = improv::build_rpc_response(
-                improv::GET_WIFI_NETWORKS, {WiFi.SSID(id), String(WiFi.RSSI(id)), (WiFi.encryptionType(id) == ENC_TYPE_NONE ? "NO" : "YES")}, false);
+                improv::GET_WIFI_NETWORKS, {WiFi.SSID(sortedIndicies[id]), String(WiFi.RSSI(sortedIndicies[id])), (WiFi.encryptionType(sortedIndicies[id]) == ENC_TYPE_NONE ? "NO" : "YES")}, false);
         send_response(data);
         delay(1);
     }
-    //final response
+    // final response
     std::vector<uint8_t> data =
         improv::build_rpc_response(improv::GET_WIFI_NETWORKS, std::vector<std::string>{}, false);
     send_response(data);
+
+    // delete scan from memory
+    WiFi.scanDelete();
 }
 
 void set_state(improv::State state) {
